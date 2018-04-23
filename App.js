8dbb0e8dff84282210af3597b3a2bdeb5dc21173
Jacob Feld <jfeld@pillartechnology.com>
2018-04-23 11:04:20 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import {StyleSheet, ScrollView, Image, Text} from 'react-native';
+
+
+class Greeting extends Component {
+    render() {
+        return (
+            <Text style={this.props.style}>Hello {this.props.name}!</Text>
+        );
+    }
+}
+
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isShowingText: true};
+
+        // Toggle the state every second
+        setInterval(() => {
+            this.setState(previousState => {
+                return {isShowingText: !previousState.isShowingText};
+            });
+        }, 100);
+    }
+
+    render() {
+        let display = this.state.isShowingText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
+        );
+    }
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <Image
+                    source={{uri: 'https://i.chzbgr.com/full/7345954048/h7E2C65F9/'}}
+                    style={{width: 320, height: 180}}
+                />
+                <Text style={styles.bigblue}>
+                    On iOS, a React Native ScrollView uses a native UIScrollView.
+                    On Android, it uses a native ScrollView.
+
+                    On iOS, a React Native Image uses a native UIImageView.
+                    On Android, it uses a native ImageView.
+
+                    React Native wraps the fundamental native components, giving you
+                    the performance of a native app, plus the clean design of React.
+                </Text>
+                <Greeting name='Paige' style={styles.red}/>
+                <Greeting name='Ben' style={styles.bigblue}/>
+                <Greeting name='Sharon' style={[styles.bigblue, styles.red]}/>
+                <Greeting name='Mark'/>
+                <Blink text='Blinking?!?!'/>
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
+});
